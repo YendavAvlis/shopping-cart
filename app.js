b53188370dd733db32030b9b923bb1e9bc6dd19d
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-04 20:12:22 +0200

    Remove Functionality Added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
     databaseURL: 'https://shopping-cart-d7284-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -25,21 +25,40 @@ addButton.addEventListener('click', () => {
 })
 
 onValue(itemsInDB, function(snapshot){
-    const itemsArray = Object.values(snapshot.val())
 
-    clearShoppingList()
+    if(snapshot.exists()){
+        const itemsArray = Object.entries(snapshot.val())
 
-    for(let i = 0; i < itemsArray.length; i++){
-        addNewItem(itemsArray[i])
+        clearShoppingList()
+
+        for(let i = 0; i < itemsArray.length; i++){
+            const currentItem = itemsArray[i]
+
+            addNewItem(currentItem)
+        }
+    } else {
+        shoppingList.textContent = 'No items here yet...'
     }
+
 })
 
 function clearInputField(){
     inputField.value = ''
 }
 
-function addNewItem(itemValue){
-    shoppingList.innerHTML += `<li>${itemValue}</li>`
+function addNewItem(item){
+
+    let newElement = document.createElement('li')
+    let itemID = item[0]
+    let itemValue = item[1]
+    newElement.textContent = `${itemValue}`
+    shoppingList.append(newElement)
+
+    newElement.addEventListener('click', () => {
+        let itemLocationInDB = ref(database, `items/${itemID}`)
+        remove(itemLocationInDB)
+
+    })
 }
 
 function clearShoppingList(){
